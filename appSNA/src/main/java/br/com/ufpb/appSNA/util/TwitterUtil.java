03e27d1ab9fc82846bffa49e4a6947e7115a781b
moacir.lopes.jr <moacir.lopes.jr@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-08-30 21:07:04 +0000

    modificação na busca dos amigos para funcionar com o twitter4j, antes estava acessando diretamente o serviço REST e realizando o parser do json

--- appSNA/src/main/java/br/com/ufpb/appSNA/util/TwitterUtil.java
+++ appSNA/src/main/java/br/com/ufpb/appSNA/util/TwitterUtil.java
@@ -3,30 +3,22 @@ package br.com.ufpb.appSNA.util;
 import java.util.LinkedList;
 import java.util.List;
 
+import twitter4j.IDs;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
-import twitter4j.internal.org.json.JSONArray;
-import twitter4j.internal.org.json.JSONObject;
 
 public class TwitterUtil {
 
-	private static final String friendsIdsRest = "http://api.twitter.com/1/friends/ids.json?screen_name=";
-	
 	public static List<User> retornarListaAmigos(String screenName) throws Exception{
 		
-		String result;
-		result = URLUtil.obterResultadoUrl(friendsIdsRest + screenName);
-		JSONObject resultJson = new JSONObject(result);
-		JSONArray arrayIds = resultJson.getJSONArray("ids");
 		Twitter twitter = createTwitterFactory().getInstance();
 		List<User> listUsers = new LinkedList<User>();
-		
+		IDs ids = twitter.getFriendsIDs(screenName, -1);
 		//TODO Funciona mas é improdutivo Ainda realiza inumeras requisições, procurar metodo ou serviço que possa retornar os dados em uma requisição
-		for(int i = 0; i < arrayIds.length(); i++){
-			String id = arrayIds.getString(i);
-			listUsers.add(twitter.showUser(Long.parseLong(id)));
+		for(long id : ids.getIDs()){
+			listUsers.add(twitter.showUser(id));
 		}
 		
 		return listUsers;

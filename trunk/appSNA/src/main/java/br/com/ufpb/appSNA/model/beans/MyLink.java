868856f453d441330f7a601e8b4c375b50492939
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-09 19:30:07 +0000

    Nova Classe MyLink

--- trunk/appSNA/src/main/java/br/com/ufpb/appSNA/model/beans/MyLink.java
+++ trunk/appSNA/src/main/java/br/com/ufpb/appSNA/model/beans/MyLink.java
@@ -6,9 +6,19 @@ public class MyLink {
 	
 	static int edgeCount = 0; 	
 	private int id;
+	private double weight;
+	private double capacity;
 
 	public MyLink() {
-		this.id = edgeCount++;		
+		this.id = edgeCount++;	
+		this.weight=1.0;
+		this.capacity=10.0;
+	}	
+	
+	public MyLink(double weight, double capacity) {
+		this.id = edgeCount++;
+		this.weight = weight;
+		this.capacity = capacity;
 	}	
 
 	public int getId() {
@@ -20,6 +30,23 @@ public class MyLink {
 		this.id = id;
 	}
 
+	
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	
+	public double getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(double capacity) {
+		this.capacity = capacity;
+	}
 
 	public String toString() {		
 		return "E" + id;

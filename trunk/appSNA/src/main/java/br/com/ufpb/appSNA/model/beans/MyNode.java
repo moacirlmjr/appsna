ae8979cbf51218db4a22e65880c962808dd8994d
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-09 18:06:22 +0000

    Nova Classe MyNode

--- trunk/appSNA/src/main/java/br/com/ufpb/appSNA/model/beans/MyNode.java
+++ trunk/appSNA/src/main/java/br/com/ufpb/appSNA/model/beans/MyNode.java
@@ -1,12 +1,18 @@
 package br.com.ufpb.appSNA.model.beans;
 
+import java.util.ArrayList;
+
 
 public class MyNode {
 
 		private int id;
+		private String nome;
+		private ArrayList<Long> listadeAmigos;
 
-		public MyNode(int id) {
+		public MyNode(int id, String nome ) {
 			this.id = id;
+			this.nome=nome;
+			this.listadeAmigos = new ArrayList<Long>();
 		}
 		
 		
@@ -19,7 +25,16 @@ public class MyNode {
 		public void setId(int id) {
 			this.id = id;
 		}
+		
+		
+		public String getNome() {
+			return nome;
+		}
 
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
 
 
 		public String toString() {

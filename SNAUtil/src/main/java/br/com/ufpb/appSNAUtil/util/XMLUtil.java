36ba8dbe04967b03f9fac691d12323ceba76a933
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-13 12:35:20 +0000

    Nova Classe XML Util

--- SNAUtil/src/main/java/br/com/ufpb/appSNAUtil/util/XMLUtil.java
+++ SNAUtil/src/main/java/br/com/ufpb/appSNAUtil/util/XMLUtil.java
@@ -6,35 +6,42 @@ import java.io.IOException;
 
 public class XMLUtil {
 	
-	//Aqui definimos o cabecalho do arquivo xml
-    public static StringBuffer arquivo = new StringBuffer(
-    		
-	    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!--  An excerpt of an egocentric social network  -->\n<graphml xmlns=\"").	
-	    append("http://graphml.graphdrawing.org/xmlns\">\n\t<graph edgedefault=\"undirected\">\n\n\t\t<!-- data schema -->" +
-	    		"\n\t\t<key id=\"name\" for=\"node\"").	
-	    append(" attr.name=\"name\" attr.type=\"string\"/>\n\t\t<key id=\"gender\" for=\"node\" attr.name=\"gender\" " +
-	    		"attr.type=\"string\"/>\n\n\t\t<!-- nodes -->  ");
+	public static StringBuffer arquivo = new StringBuffer("<?xml version=\"1.0\" encoding=\"UTF-8\"?>").
+			append("\n<!-- GraphML gerado automaticamente pela AppSNA -->").
+			append("\n<graphml xmlns=\"http://graphml.graphdrawing.org/xmlns\">");
+	
+	
+	//Método utilizado para gerar o cabecalho do XML
+	public void generateHeader(boolean isDirected){		
+		arquivo.append("\n\t<graph edgedefault=\"").append(isDirected == true ? "Directed" : "Undirected").append("\">").
+				append("\n\n\t\t<!-- data schema -->").
+				append("\n\t\t<key id=\"name\" for=\"node\" attr.name=\"name\" attr.type=\"string\"/>").
+				append("\n\t\t<key id=\"gender\" for=\"node\" attr.name=\"gender\" attr.type=\"string\"/>").
+				append("\n\n\t\t<!-- nodes -->  ");			
+	}
 
    
 
     //Método utilizado para gerar os nodos, será passado um id, para correlação nas arestas.    
-    public static void generateNodes(long userId, String name, String gender){
-    	
-           arquivo.append("\n\t\t<node id=\"").append(userId).append("\">\n\t\t\t").append(
-           "<data key=\"name\">").append(name).append("</data>\n\t\t\t<data key=\"gender\">").append(gender).append("</data>\n\t\t</node>");
+    public static void generateNodes(long userId, String name, String gender){    	
+           arquivo.append("\n\t\t<node id=\"").append(userId).append("\">\n\t\t\t").
+           append("<data key=\"name\">").append(name).append("</data>").
+           append("\n\t\t\t<data key=\"gender\">").append(gender).append("</data>\n\t\t</node>");
     }
     
     //Método sobrescrito.    
     public static void generateNodes(long userId, String name){
-    	   arquivo.append("\n\t\t<node id=\"").append(userId).append("\">\n\t\t\t").append(
-           "<data key=\"name\">").append(name).append("</data>\n\t\t</node>");
+    	   arquivo.append("\n\t\t<node id=\"").append(userId).append("\">").
+    	   append("\n\t\t\t<data key=\"name\">").append(name).append("</data>").
+    	   append("\n\t\t</node>");
     }
 
    
 
     //Nessa método serão construídos as arestas correspondes ao id do usuário de origem (idSource) ao id do usuário de destino (idTarget).
     public static void generateEdges(long idSource, long idTarget){
-           arquivo.append("\n\t\t<edge source=\"").append(idSource).append("\" target=\"").append(idTarget).append("\"></edge>\n\t\t\t");
+           arquivo.append("\n\t\t<edge source=\"").append(idSource).append("\" target=\"").append(idTarget).
+           append("\"></edge>\n\t\t\t");
     }
 
     

a3f2a9c53855650e6ee0242f1f7e55ee8da306c8
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-19 19:58:31 +0000

    E

--- trunk/SNAUtil/src/main/java/br/com/ufpb/appSNAUtil/util/Constantes.java
+++ trunk/SNAUtil/src/main/java/br/com/ufpb/appSNAUtil/util/Constantes.java
@@ -1,5 +1,7 @@
 package br.com.ufpb.appSNAUtil.util;
 
+import java.io.File;
+
 public class Constantes {
 
 	public static final String ipRedis = "200.162.143.40";
@@ -9,6 +11,6 @@ public class Constantes {
 	//windows
 	public static final String DIR_APPSNA = "C:\\appsna\\";
 	
-	public static final String DIR_DESKTOP_DANYLLO = "C:\\Users\\User\\Desktop\\teste99.csv";
+	public static final String DIR_DESKTOP_DANYLLO = "C:"+ File.separator +"Users"+ File.separator +"User"+ File.separator +"Desktop"+ File.separator +"teste99.csv";
 	public static final String DIR_DESKTOP_VIRTUAL = "C:\\Users\\David\\Desktop\\teste99.csv";
 }

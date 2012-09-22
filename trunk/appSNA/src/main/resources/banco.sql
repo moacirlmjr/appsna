fd6dc261424514e2996179af07828c9e5dd01a40
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-22 21:08:48 +0000

    novo BAnco

--- trunk/appSNA/src/main/resources/banco.sql
+++ trunk/appSNA/src/main/resources/banco.sql
@@ -26,3 +26,49 @@ primary key(id_source, id_target),
 constraint foreign key(id_source) references elemento(id),
 constraint foreign key(id_target) references elemento(id)
 );
+
+
+create table status(
+id bigint not null auto_increment,
+id_elemento bigint not null,
+dataDeCriacao timestamp,
+texto text not null,
+latitude bigint,
+longitude bigint,
+totalRetweet int,
+isRetweeted bit,
+primary key(id, id_elemento),
+constraint foreign key(id_elemento) references elemento(id)
+);
+
+create table usermention(
+id bigint not null auto_increment,
+id_status bigint not null,
+id_elemento bigint not null,
+mention text,
+primary key(id, id_status, id_elemento),
+foreign key(id_status) references tweet(id),
+foreign key(id_elemento) references tweet(id_elemento)
+);
+
+create table urlmention(
+id bigint not null auto_increment,
+id_status bigint not null,
+id_elemento bigint not null,
+url text,
+primary key(id, id_status, id_elemento),
+foreign key(id_status) references tweet(id),
+foreign key(id_elemento) references tweet(id_elemento)
+);
+
+
+create table hashtagmention(
+id bigint not null auto_increment,
+id_status bigint not null,
+id_elemento bigint not null,
+hashtag text,
+primary key(id, id_status, id_elemento),
+foreign key(id_status) references tweet(id),
+foreign key(id_elemento) references tweet(id_elemento)
+);
+

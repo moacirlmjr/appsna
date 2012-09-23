96e90191f4e4c97729fcb30089578de365b4a26d
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-23 13:09:48 +0000

    Novos DAO`s

---
+++ appSNA/src/main/java/br/com/ufpb/appSNA/model/dao/HashTagMentionDAO.java
@@ -0,0 +1,8 @@
+package br.com.ufpb.appSNA.model.dao;
+
+import br.com.ufpb.appSNA.model.beans.HashTagMention;
+import br.com.ufpb.appSNAUtil.model.dao.AppSnaDAO;
+
+public interface HashTagMentionDAO extends AppSnaDAO<HashTagMention> {
+
+}
---
+++ appSNA/src/main/java/br/com/ufpb/appSNA/model/dao/HashTagMentionDAOImpl.java
@@ -0,0 +1,45 @@
+package br.com.ufpb.appSNA.model.dao;
+
+import java.util.List;
+
+import br.com.ufpb.appSNA.model.beans.HashTagMention;
+
+public class HashTagMentionDAOImpl implements HashTagMentionDAO {
+
+	@Override
+	public Long create(HashTagMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(List<HashTagMention> objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Long update(HashTagMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HashTagMention findById(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<HashTagMention> list() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(HashTagMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

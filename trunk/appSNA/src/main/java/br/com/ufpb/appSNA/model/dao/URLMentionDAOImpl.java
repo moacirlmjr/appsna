237e2190f0f82f7906668073650d692dba4e5a1c
danyllowagneralbuquerque <danyllowagneralbuquerque@6bed9825-1842-74aa-c7f0-b973e1e8023c>
2012-09-23 13:09:48 +0000

    Novos DAO`s

---
+++ trunk/appSNA/src/main/java/br/com/ufpb/appSNA/model/dao/URLMentionDAOImpl.java
@@ -0,0 +1,45 @@
+package br.com.ufpb.appSNA.model.dao;
+
+import java.util.List;
+
+import br.com.ufpb.appSNA.model.beans.URLMention;
+
+public class URLMentionDAOImpl implements URLMentionDAO {
+
+	@Override
+	public Long create(URLMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(List<URLMention> objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Long update(URLMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public URLMention findById(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<URLMention> list() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(URLMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/appSNA/src/main/java/br/com/ufpb/appSNA/model/dao/UserMentionDAOImpl.java
@@ -0,0 +1,45 @@
+package br.com.ufpb.appSNA.model.dao;
+
+import java.util.List;
+
+import br.com.ufpb.appSNA.model.beans.UserMention;
+
+public class UserMentionDAOImpl implements UserMentionDAO {
+
+	@Override
+	public Long create(UserMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(List<UserMention> objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Long update(UserMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserMention findById(Long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<UserMention> list() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(UserMention objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
